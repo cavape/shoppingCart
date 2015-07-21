fe06c29ca735b76a118c586b2f6bf5484aa318e4
Your NameCarlos <carlos@Carloss-MacBook-Pro.local>
2015-07-21 11:11:39 -0400

    My first commit

--- js/app.js
+++ js/app.js
@@ -8,8 +8,18 @@ $(document).ready(function(){
            
         success: function (data) {
             console.log(data);
-            var img = data.shirts[0].image
-            $('.dummy-grid__item').html('<img src="' + img + '">');
+            
+            var insertImage =" ";
+            for (var i=0; i<data.shirts.length; i++ ){
+            	 var img = data.shirts[i].image
+            	insertImage += '<div class="dummy-grid__item"><img src="' + img + '"></div>';
+            	 $('.dummy-grid').html(insertImage);
+            }
+           
+            // $('.dummy-grid__item').html('<img src="' + img + '">');
+
+            // var img = data.shirts[0].image
+            // $('.dummy-grid__item').html('<img src="' + img + '">');
         
     	},
     	error:function(){
