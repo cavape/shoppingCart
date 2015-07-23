a12cc10a884ddaef7c267095615a7f38a85240c1
Your NameCarlos <carlos@Carloss-MacBook-Pro.local>
2015-07-23 14:13:34 -0400

    my second commit

--- js/app.js
+++ js/app.js
@@ -1,45 +1,72 @@
 $(document).ready(function(){
+
 	    shirts_call();
+	    
+	  
 });
 
+// run ajax request Shirts
 function shirts_call(){
-// run ajax request
+
     $.ajax({
     	url: "http://174.129.248.23/brainstation/shop/shirts",
         type: "GET",
         dataType: "jsonp",
            
         success: function (data) {
-            console.log(data);
             
-            var insertData =" ";
-            for (var i=0; i<data.shirts.length; i++ ){
+            
+            var insertData ='';
+            var len =data.shirts.length;
+            // console.log(len);
+
+            for (var i=0; i<len; i++ ){
+                
             	 var img = data.shirts[i].image;
             	 var NameItem = data.shirts[i].name;
             	 var prc = data.shirts[i].price;
+                 var frm ='Quantity: <input type="number" name="quantity" min="1" max="99"><button class="BuyNow" type="submit">Add to Cart</button></div>'
+
             	insertData += '<div class="dummy-grid__item"><img src="' + img + '">';
             	insertData += '<div class="NameProd">'+ NameItem +'</div>';
-            	insertData += '<div class="PriceProd">'+ '$ ' + prc + '</div>'
+            	insertData += '<div class="PriceProd">'+ '$ ' + prc + '</div>';
+                insertData += frm; +'</div>'
 
-            	insertData += '<form><div>Quantity: <input type="number" name="quantity" min="1" max="99"></div>';
-            	insertData += '<button class="BuyNow">Add to Cart</button></form></div>'
+                //I have to removed <form> is causing problems
 
-            	$('.dummy-grid').html(insertData);
+                 $('.dummy-grid').html(insertData);
+
+            	
+				$('.dummy-grid__item').on('click','.BuyNow', function(){
+			    var myIndex = $(this).index('.BuyNow');                
+			    var addName = $('.NameProd')[myIndex];
+
+                console.log(addName);
+			    // var addQuantity = $(this).find(input).val();
+			    // console.log(addName);
+       //          $('.checkOutName').html(addName);
+			    // console.log(addQuantity);
+			    });
+
             }
+         
         
     	},
     	error:function(){
             $('dummy-grid__item').html("There was an error communicating with the server");
             } 
 	});
+
 }
 
-/////Click Shirts
+
+
+//Click Shirts
 $('.selectShirt').click(function(){
 		shirts_call();
 });
 
-/////Click Pants
+//Click Pants
 $('.selectPants').click(function(){
 
 	    // run ajax request
@@ -68,9 +95,9 @@ $('.selectPants').click(function(){
             } 
 	});
 });
-///////
 
-/////Click Shoes
+
+//Click Shoes
 $('.selectShoes').click(function(){
 
 	    // run ajax request
@@ -99,7 +126,7 @@ $('.selectShoes').click(function(){
             } 
 	});
 });
-///////
+
 
 
 
